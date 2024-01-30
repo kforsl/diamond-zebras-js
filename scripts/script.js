6d57b6c33b6b8319c2c01931dca6d12c047a0674
Kim Forslund <138223196+kforsl@users.noreply.github.com>
2024-01-30 09:31:08 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,24 @@ window.addEventListener('load', () => {
 
 });
 
+validateNotAfraid()
+
+//tar input med id 'question'
+const checkbox = document.querySelector('#question');
+
+const notAfraidGhosts = true;
+console.log(notAfraidGhosts);
+
+
+//lägg til händelse
+checkbox.addEventListener('click', function () {
+    if (checkbox.checked) {
+        console.log('Spelare är inte rädd');
+    } else {
+        console.log('Spelare är rädd');
+    }
+});
+
 
 function initGame() {
     // Döljer Formulär när man loggat in. 
